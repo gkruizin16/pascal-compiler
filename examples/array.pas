19daba897fa00591d88b7e31828ffaf7ef1f9317
Geert Jan Kruizinga <kruizinga.g@hsleiden.nl>
2017-11-02 17:31:33 +0100

    Wijziging 2 nov 17:31

--- examples/array.pas
+++ examples/array.pas
@@ -3,8 +3,8 @@ var a1: array[20..30] of integer;
 var c1: array['a'..'e'] of integer;
 
 begin
-    a1[23] := 19;
-    a1[24] := a1[23] + 33;
+    a1[23] := 1730;
+    a1[24] := a1[23] + 1;
     writeln(a1[24]);
 
     c1['b'] := 10;
